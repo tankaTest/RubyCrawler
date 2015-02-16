3a8bb000846b9978a2bc1f7612d318584c5a4881
tankaTest <selis@hotbox.ru>
2015-02-16 21:10:00 +0200

    Create test.rb

---
+++ test.rb
@@ -0,0 +1,13 @@
+require 'net/http'
+require "net/https"
+require "uri"
+
+uri = URI.parse('https://www.bondora.com')
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(uri.request_uri)
+
+response = http.request(request)
+puts response.body
