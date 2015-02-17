19071ac3187ec749828482f7dbbbb6fcbf38e3cf
tankaTest <selis@hotbox.ru>
2015-02-17 22:52:42 +0200

    Update test.rb
    - UTF8 support (euro currency sign)
    - extracting prices by regexp
    - exceptions handling for URI parsing

--- test.rb
+++ test.rb
@@ -1,3 +1,4 @@
+# encoding=utf-8 
 require 'net/http'
 require "net/https"
 require "uri"
@@ -10,16 +11,29 @@ http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 request = Net::HTTP::Get.new(uri.request_uri)
 
 response = http.request(request)
-#puts response.body
+puts response.body
+respBody = response.body.force_encoding("UTF-8")
+p respBody.scan(/\u20AC[\d,\.,\,]+\d+/)
+p respBody.scan(/\u20AC([\d,\.,\,]+\d+)/)
+
 arrayContent = URI.extract(response.body, ['http', 'https'])
+
+
 
 for element in arrayContent do
 	begin
 	puts element
 	uri = URI.parse(element)
+	puts 'URI host: ' + uri.host
+	if 'www.bondora.ee' == uri.host
+		puts 'OK to follow URI: ' 
+	else 				
+		puts 'External URI: ' 
+	end
 	rescue 
     # Error found
-    puts "Error found during parsing  (URI::InvalidURIError)"
+    #puts "Error found during parsing  (URI::InvalidURIError)"
 	ensure
 	end
 end
+
