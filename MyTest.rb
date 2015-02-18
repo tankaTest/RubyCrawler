ff2ae1d626c44cd84f77f134ab503dda877cdf4f
tankaTest <selis@hotbox.ru>
2015-02-18 21:24:01 +0200

    Update MyTest.rb
    Concat strings to get new uri

--- MyTest.rb
+++ MyTest.rb
@@ -1,32 +1,29 @@
 require "net/https"
 require "uri"
-	
-uri = URI.parse("https://www.bondora.com/")
+require 'pathname'
+
+home_uri="https://www.bondora.com/"
+uri = URI.parse(home_uri)
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 request = Net::HTTP::Get.new(uri.request_uri)
-
 response = http.request(request)
-#puts response.body
-#puts response.status
 
-regex=/(href="\/[^"]+")/
+regex=/href="\/([^"]+)"/
 arrayLink = response.body.scan(regex)
 arrayLinkUniq = arrayLink.uniq
-#puts linksArray
 
 
 
 for element in arrayLinkUniq do
 	begin
-	puts element
-	
+	new_uri="#{uri}#{element[0]}"
+	puts "#{uri}#{element[0]}"
 	rescue 
     # Error found
     #puts "Error found during parsing  (URI::InvalidURIError)"
 	ensure
 	end
 end
-
