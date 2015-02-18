80c32e9212534f7a60bbca01854d63ef5e26b266
tankaTest <selis@hotbox.ru>
2015-02-18 22:07:58 +0200

    Update MyTest.rb
    Recursion search completed with depth restriction

--- MyTest.rb
+++ MyTest.rb
@@ -1,29 +1,40 @@
 require "net/https"
 require "uri"
 require 'pathname'
+$i=0
+
+def visit_url(home_uri)
+	$i+=1
+	uri = URI.parse(home_uri)
+	http = Net::HTTP.new(uri.host, uri.port)
+	http.use_ssl = true
+	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+	request = Net::HTTP::Get.new(uri.request_uri)
+	response = http.request(request)
+
+	regex=/href="\/([^"]+)"/
+	arrayLink = response.body.scan(regex)
+	arrayLinkUniq = arrayLink.uniq
+
+	for element in arrayLinkUniq do
+		begin
+		new_uri="#{uri}#{element[0]}"
+		puts "#{uri}#{element[0]}"
+		#visit_url (new_uri)
+		
+if $i<3 then 
+visit_url (new_uri)
+end
+		rescue 
+		# Error found
+		#puts "Error found during parsing  (URI::InvalidURIError)"
+		ensure
+		end
+	end
+$i-=1
+end
+
 
 home_uri="https://www.bondora.com/"
-uri = URI.parse(home_uri)
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-request = Net::HTTP::Get.new(uri.request_uri)
-response = http.request(request)
-
-regex=/href="\/([^"]+)"/
-arrayLink = response.body.scan(regex)
-arrayLinkUniq = arrayLink.uniq
-
-
-
-for element in arrayLinkUniq do
-	begin
-	new_uri="#{uri}#{element[0]}"
-	puts "#{uri}#{element[0]}"
-	rescue 
-    # Error found
-    #puts "Error found during parsing  (URI::InvalidURIError)"
-	ensure
-	end
-end
+visit_url (home_uri)
