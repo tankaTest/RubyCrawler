d366cf91b2fd78c0c1e5d9c953b9e969ac06cdc2
tankaTest <selis@hotbox.ru>
2015-02-18 16:55:13 +0200

    Update MyTest.rb
    display only unique strings

--- MyTest.rb
+++ MyTest.rb
@@ -1,6 +1,6 @@
 require "net/https"
 require "uri"
-
+	
 uri = URI.parse("https://www.bondora.com/")
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
@@ -12,18 +12,21 @@ response = http.request(request)
 #puts response.body
 #puts response.status
 
-regex=/href="([^"]+)"/
+regex=/(href="\/[^"]+")/
 arrayLink = response.body.scan(regex)
+arrayLinkUniq = arrayLink.uniq
 #puts linksArray
 
 
 
-for element in arrayLink do
+for element in arrayLinkUniq do
 	begin
 	puts element
+	
 	rescue 
     # Error found
     #puts "Error found during parsing  (URI::InvalidURIError)"
 	ensure
 	end
 end
+
