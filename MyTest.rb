658e581858b382fcfbb21cc467acc7b72adfb96d
tankaTest <selis@hotbox.ru>
2015-02-19 21:28:21 +0200

    Update MyTest.rb
    Unique full array received

--- MyTest.rb
+++ MyTest.rb
@@ -1,15 +1,15 @@
 require "net/https"
 require "uri"
-require 'pathname'
 require 'set'
 
 $i = 0
 $recursionArray = Set.new []
 p $recursionArray
 
-def visit_url(home_uri)
+def visit_url(new_uri)
 	$i += 1
-	uri = URI.parse(home_uri)
+	home_uri="https://www.bondora.com/"
+	uri = URI.parse(new_uri)
 	http = Net::HTTP.new(uri.host, uri.port)
 	http.use_ssl = true
 	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -18,27 +18,38 @@ def visit_url(home_uri)
 	response = http.request(request)
 
 	regex = /href="\/([^"]+)"/
+	
+	#puts "Response body:"
+	#puts response.body.scan(regex).uniq
+	arrayLink = []
+	harrayLinkUniq = []
 	arrayLink = response.body.scan(regex)
 	arrayLinkUniq = arrayLink.uniq
 	
+	puts "Full array: "
+	p arrayLinkUniq
+	
+
 			
 	for element in arrayLinkUniq do
 		begin
-		new_uri = "#{uri}#{element[0]}"
+		new_uri = "#{home_uri}#{element[0]}"
+		#puts "URI takoj:"
+		#puts uri
+		#puts element
+		#puts "New URI to visit:"
 		#puts new_uri
 		#visit_url (new_uri)
 		
 		#Create unique array of links during recursion
 		#puts $recursionArray.include?(new_uri)
 		if $recursionArray.include?(new_uri) == false then
-		$recursionArray.add(new_uri)
-		#puts $recursionArray.include?(new_uri)
-		#puts $recursionArray
-		end
-		
+		#$recursionArray.add(new_uri)
 		#recursion depth limit to exit from cycle
-		if $i<6 then 
-		visit_url (new_uri)
+			if $i<6 then 
+			visit_url (new_uri)
+			end
+		$recursionArray.add(new_uri)
 		end
 		
 		rescue 
@@ -55,5 +66,5 @@ end
 home_uri="https://www.bondora.com/"
 visit_url (home_uri)
 
-#$recursionArray.each{ |i| puts i } 
+$recursionArray.each{ |i| puts i } 
 puts $recursionArray.length
