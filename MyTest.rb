8eac6afde7a732c4e67595a767a04e8239bff639
tankaTest <selis@hotbox.ru>
2015-02-19 14:00:24 +0200

    Update MyTest.rb
    add unique links to hashArray and display after recursion completed

--- MyTest.rb
+++ MyTest.rb
@@ -1,10 +1,14 @@
 require "net/https"
 require "uri"
 require 'pathname'
-$i=0
+require 'set'
+
+$i = 0
+$recursionArray = Set.new []
+p $recursionArray
 
 def visit_url(home_uri)
-	$i+=1
+	$i += 1
 	uri = URI.parse(home_uri)
 	http = Net::HTTP.new(uri.host, uri.port)
 	http.use_ssl = true
@@ -13,23 +17,35 @@ def visit_url(home_uri)
 	request = Net::HTTP::Get.new(uri.request_uri)
 	response = http.request(request)
 
-	regex=/href="\/([^"]+)"/
+	regex = /href="\/([^"]+)"/
 	arrayLink = response.body.scan(regex)
 	arrayLinkUniq = arrayLink.uniq
-
+	
+			
 	for element in arrayLinkUniq do
 		begin
-		new_uri="#{uri}#{element[0]}"
-		puts "#{uri}#{element[0]}"
+		new_uri = "#{uri}#{element[0]}"
+		#puts new_uri
 		#visit_url (new_uri)
 		
-if $i<3 then 
-visit_url (new_uri)
-end
+		#Create unique array of links during recursion
+		#puts $recursionArray.include?(new_uri)
+		if $recursionArray.include?(new_uri) == false then
+		$recursionArray.add(new_uri)
+		#puts $recursionArray.include?(new_uri)
+		#puts $recursionArray
+		end
+		
+		#recursion depth limit to exit from cycle
+		if $i<6 then 
+		visit_url (new_uri)
+		end
+		
 		rescue 
-		# Error found
+		# Skip if Error found
 		#puts "Error found during parsing  (URI::InvalidURIError)"
 		ensure
+		#continue operation
 		end
 	end
 $i-=1
@@ -38,3 +54,6 @@ end
 
 home_uri="https://www.bondora.com/"
 visit_url (home_uri)
+
+#$recursionArray.each{ |i| puts i } 
+puts $recursionArray.length
