f43f489004e97a6d47e7290361e5f44a6f9d84ba
tankaTest <selis@hotbox.ru>
2015-02-21 21:16:44 +0200

    Update search_word.rb
    loan found

--- search_word.rb
+++ search_word.rb
@@ -1,14 +1,8 @@
 require 'nokogiri'
 require "net/http"
-require "uri"
-require 'open-uri'
-require 'rubygems'
 require "net/https"
-require "uri"
-require 'set'
-
 	
-	home_uri="https://www.bondora.com/en/loan/"
+	home_uri="https://www.bondora.com/en/loan/loan-application"
 	uri = URI.parse(home_uri)
 	http = Net::HTTP.new(uri.host, uri.port)
 	http.use_ssl = true
@@ -22,30 +16,12 @@ response = response.body
 page = Nokogiri::HTML(response)
 page.css('script, link, title').each { |node| node.remove }
 
-
-#array1 = page.css('body').inner_text.scan(/.{0,20}loan.{0,20}/)
-#array2 = page.css('body').inner_text.scan(/.{0,20}Loan.{0,20}/)
-array1 = page.css('body').inner_text.scan(/loan/)
-array2 = page.css('body').inner_text.scan(/Loan/)
 array3 = page.css('body').inner_text.scan(/(?i)loan/)
-array4= page.css('body').inner_text.scan(/LOAN/)
-
-puts "loan count:"
-#puts array1
-#array1.each{ |i| puts i } 
-puts array1.length
-
-puts "Loan count:"
-#puts array2
-#array2.each{ |i| puts i } 
-puts array2.length
 
 puts "Loan case insensitive:"
-puts array3
-#array2.each{ |i| puts i } 
+#puts array3
 puts array3.length
 
-#puts array4.length
 
 
 
