0cd0181c1b5694818cc06dc983307dc41523231c
tankaTest <selis@hotbox.ru>
2015-02-21 21:59:19 +0200

    Create currency_found.rb

---
+++ currency_found.rb
@@ -0,0 +1,7 @@
+/\u20AC[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?/
+
+\u20AC euro
+[+-]? optional +-
+[0-9]{1,3}   1-3 digits 
+(?:,?[0-9]{3})*   mozhet byt' "," ili 1-3 digits - skolko ugodno raz
+(?:\.[0-9]{2})?   centovaja optional group
