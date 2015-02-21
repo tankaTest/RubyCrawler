0ff773f17c8ae094d2b9b656b005d48ae67f315f
tankaTest <selis@hotbox.ru>
2015-02-21 22:13:55 +0200

    Update currency_found.rb
    currency found and parsed

--- currency_found.rb
+++ currency_found.rb
@@ -1,10 +1,18 @@
+# encoding=utf-8 
+require 'net/http'
+require "net/https"
+require "uri"
 
-http://stackoverflow.com/questions/354044/what-is-the-best-u-s-currency-regex
+uri = URI.parse('https://www.bondora.com/en/fees/')
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-/\u20AC[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?/
+request = Net::HTTP::Get.new(uri.request_uri)
 
-\u20AC euro
-[+-]? optional +-
-[0-9]{1,3}   1-3 digits 
-(?:,?[0-9]{3})*   mozhet byt' "," ili 1-3 digits - skolko ugodno raz
-(?:\.[0-9]{2})?   centovaja optional group
+response = http.request(request)
+respBody = response.body.force_encoding("UTF-8")
+
+test_price=respBody.scan(/\u20AC[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?/)
+p test_price
+test_price.each{ |i| puts i.gsub(/[^\d\.]/, '').to_f }
